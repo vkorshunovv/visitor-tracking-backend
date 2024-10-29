5d95a0004d48fbfa6433a7f97ba16530d4d10e6f
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-10-29 11:43:49 +0100

    migrate Visitor model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Visitor(models.Model):
+    ip = models.GenericIPAddressField()
+    user_agent = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.ip} at {self.timestamp}"
+    
